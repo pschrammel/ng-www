5684a7ac2a3e7d7fd3be3da681cec24e41e7ee9e
Peter Schrammel <peter.schrammel@preisanalytics.de>
2018-06-21 14:14:52 +0200

    after upgrade

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,3 @@
 <template>
-<h1>Looks Good</h1>
+  <h1>Looks Good</h1>
 </template>
--- components/Text.vue
+++ components/Text.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-Lots of funny text, just to show that.
-Even long texts may be rendered. And can flow correctly.
-</div>
+  <div>
+    Lots of funny text, just to show that.
+    Even long texts may be rendered. And can flow correctly.
+  </div>
 </template>
