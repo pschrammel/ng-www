9255f5f806e571038e3bffd5e3f49df5a08ce260
Peter Schrammel <peter.schrammel@preisanalytics.de>
2018-09-12 13:32:38 +0000

    fixing test data

--- content/main.js
+++ content/main.js
@@ -7,6 +7,7 @@ export default {
     vertical_compact: true,
     margins: [10,10],
     use_css_transforms: true,
+    i: 'main',
     layout: [
       {"x":0,"y":0,"w":1,"h":2,"i":"0",type: "gheader", data: {text: "Yeah baby"}},
       {"x":1,"y":0,"w":1,"h":4,"i":"1",type: "gtext", data: {text: "text"}},
@@ -20,11 +21,12 @@ export default {
        data: {
          col_num: 1,
          row_height: 40,
-         is_dragable: true,
+         is_draggable: true,
          is_resizable: true,
          vertical_compact: true,
          margins: [20,20],
          use_css_transforms: true,
+         i: 'sub',
          layout: [
            {"x":0,"y":0,"w":2,"h":4,"i":"10",type: "gheader", data: {text: "Special1"}},
            {"x":0,"y":1,"w":2,"h":4,"i":"11",type: "gheader", data: {text: "Special2"}}
