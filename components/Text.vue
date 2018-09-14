b2ad7cc0c4c3b6374dc20ed1a8e11c516849a773
Peter Schrammel <peter.schrammel@preisanalytics.de>
2018-09-14 06:52:56 +0000

    simple editing of textarea

--- components/Text.vue
+++ components/Text.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>
-    {{data.text}}
+  <div style="height: 100%">
+    <div v-if="ctx.editing" class="editing" >
+      <i v-if="true" class="glyphicon glyphicon-option-vertical" aria-hidden="true"></i>
+      <i v-else="false" class="glyphicon glyphicon-pencil" aria-hidden="true"></i>
+    </div>
+    <textarea v-model="data.text" v-if="ctx.editing" @blur="save" style="width: 100%; height: 100%; background-color: transparent; border: 0">
+    </textarea>
+    <template v-if="!ctx.editing">
+      {{data.text}}
+    </template>
   </div>
 </template>
+<style scoped>
+  .editing {
+    position: absolute;
+    right: 4px;
+    top: 2px;
+  }
+</style>
 
 <script>
  export default {
@@ -16,6 +31,11 @@
      ctx: {
        type: Object,
      },
+   },
+   methods: {
+     save() {
+       console.log("saving text:", this.data.text )
+     }
    }
  }
 </script>
